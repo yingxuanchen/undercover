fefce910b6aa27f8f4e27c2bbce28e7fcdb311bd
Chen Yingxuan <yingxuanchen@hotmail.com>
2025-05-19 10:42:10 +0800

    make app work in both browser light and dark theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,20 +4,37 @@ import Room from "./pages/Room";
 import MainPage from "./pages/MainPage";
 import { SnackbarProvider } from "./stores/snackbarStore";
 import { GameStateProvider } from "./stores/gameStore";
+import { createTheme, CssBaseline, ThemeProvider, useMediaQuery } from "@mui/material";
+import { useMemo } from "react";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
   return (
-    <SnackbarProvider>
-      <GameStateProvider>
-        <HashRouter>
-          <Routes>
-            <Route path="/room" element={<Room />} />
-            <Route path="/" element={<MainPage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </HashRouter>
-      </GameStateProvider>
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <SnackbarProvider>
+        <GameStateProvider>
+          <HashRouter>
+            <Routes>
+              <Route path="/room" element={<Room />} />
+              <Route path="/" element={<MainPage />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </HashRouter>
+        </GameStateProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
 
