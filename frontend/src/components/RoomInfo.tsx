3b5d877d36e05d313cc530eb8bccf9abdb653f48
Chen Yingxuan <yingxuanchen@hotmail.com>
2025-05-21 10:59:25 +0800

    try fix tooltip not working in mobile

--- frontend/src/components/RoomInfo.tsx
+++ frontend/src/components/RoomInfo.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { gameStore } from "../stores/gameStore";
-import { Button, Checkbox, FormControlLabel, Stack, Tooltip } from "@mui/material";
+import { Button, Checkbox, ClickAwayListener, FormControlLabel, Stack, Tooltip } from "@mui/material";
 import AccessibilityNewIcon from "@mui/icons-material/AccessibilityNew";
 import { getMinMaxAntiBlank } from "../shared/utils";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
@@ -32,6 +32,7 @@ export default function RoomInfo({ roleCounts, handleRoleCountsChange }: Props) 
   const { state: gameState } = useContext(gameStore);
   const { room, user } = gameState;
   const [roleList, setRoleList] = useState<Role[]>([]);
+  const [openTooltip, setOpenTooltip] = useState(false);
 
   if (!room || !user) {
     return <>No data</>;
@@ -78,25 +79,30 @@ export default function RoomInfo({ roleCounts, handleRoleCountsChange }: Props) 
     handleRoleCountsChange(role === roles[RoleType.normal] ? antiCount + 1 : antiCount - 1, blankCount);
   };
 
+  const handleTooltipClose = () => {
+    setOpenTooltip(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpenTooltip(true);
+  };
+
   return (
-    <Stack spacing={1}>
+    <Stack spacing={1} alignItems="center">
       <div>Room Id: {room.roomId}</div>
-      <div style={{ display: "flex" }}>
-        <Tooltip
-          title={
-            <>
-              Undercover: min {minAnti}, max {maxAnti}
-              <br />
-              Blank: min {minBlank}, max {maxBlank}
-            </>
-          }
-        >
-          <Button>
-            <HelpOutlineIcon fontSize="small" />
-          </Button>
-        </Tooltip>
+
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          overflow: "visible",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "10px 5px",
+        }}
+      >
         {roleList.map((role, i) => (
-          <span key={i} style={{ display: "inline-block", margin: "0 5px" }}>
+          <span key={i}>
             <AccessibilityNewIcon fontSize="large" sx={{ color: role.color }} onClick={() => handleRoleChange(role)} />
             <div style={{ fontSize: "14px" }}>{role.label}</div>
           </span>
@@ -104,11 +110,38 @@ export default function RoomInfo({ roleCounts, handleRoleCountsChange }: Props) 
       </div>
 
       {!room.hasStarted && user.isHost && (
-        <FormControlLabel
-          control={<Checkbox checked={blankCount === 1} onChange={handleBlankChange} name="blank" />}
-          label="Include blank?"
-          disabled={room.totalCount < 4}
-        />
+        <span>
+          <FormControlLabel
+            control={<Checkbox checked={blankCount === 1} onChange={handleBlankChange} name="blank" />}
+            label="Include blank?"
+            disabled={room.totalCount < 4}
+          />
+          <ClickAwayListener onClickAway={handleTooltipClose}>
+            <Tooltip
+              onClose={handleTooltipClose}
+              open={openTooltip}
+              disableFocusListener
+              disableHoverListener
+              disableTouchListener
+              title={
+                <>
+                  Undercover: min {minAnti}, max {maxAnti}
+                  <br />
+                  Blank: min {minBlank}, max {maxBlank}
+                </>
+              }
+              slotProps={{
+                popper: {
+                  disablePortal: true,
+                },
+              }}
+            >
+              <Button onClick={handleTooltipOpen} size="small" variant="text" sx={{ minWidth: "0px" }}>
+                <HelpOutlineIcon fontSize="small" />
+              </Button>
+            </Tooltip>
+          </ClickAwayListener>
+        </span>
       )}
     </Stack>
   );
